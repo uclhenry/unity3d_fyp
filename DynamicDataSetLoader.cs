d181f474f08f8c6d74e95fbff6af5c7d4269a953
uclhenry <yuhangxuhenry@gmail.com>
2017-11-15 21:57:14 +0000

    动态载入dataset并设置生成文本

---
+++ DynamicDataSetLoader.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Vuforia;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DynamicDataSetLoader : MonoBehaviour
+{
+    //public bool once = false;
+    // specify these in Unity Inspector
+    public GameObject augmentationObject = null;  // you can use teapot or other object
+    public string dataSetName = "";  //  Assets/StreamingAssets/QCAR/DataSetName
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        // Vuforia 6.2+
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(LoadDataSet);
+    }
+
+    void LoadDataSet()
+    {
+
+        ObjectTracker objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+
+        DataSet dataSet = objectTracker.CreateDataSet();
+
+        if (dataSet.Load(dataSetName))
+        {
+
+            objectTracker.Stop();  // stop tracker so that we can add new dataset
+
+            if (!objectTracker.ActivateDataSet(dataSet))
+            {
+                // Note: ImageTracker cannot have more than 100 total targets activated
+                Debug.Log("<color=yellow>Failed to Activate DataSet: " + dataSetName + "</color>");
+            }
+
+            if (!objectTracker.Start())
+            {
+                Debug.Log("<color=yellow>Tracker Failed to Start.</color>");
+            }
+
+            int counter = 0;
+
+            IEnumerable<TrackableBehaviour> tbs = TrackerManager.Instance.GetStateManager().GetTrackableBehaviours();
+            foreach (TrackableBehaviour tb in tbs)
+            {
+                if (tb.name == "New Game Object")
+                {
+
+                    // change generic name to include trackable name
+                    tb.gameObject.name = ++counter + ":DynamicImageTarget-" + tb.TrackableName;
+
+                    // add additional script components for trackable
+                    tb.gameObject.AddComponent<DefaultTrackableEventHandler>();
+                    tb.gameObject.AddComponent<TurnOffBehaviour>();
+
+                    if (augmentationObject != null)
+                    {
+                        // instantiate augmentation object and parent to trackable
+                        GameObject augmentation = (GameObject)GameObject.Instantiate(augmentationObject);
+                        augmentation.transform.parent = tb.gameObject.transform;
+                        augmentation.transform.localPosition = new Vector3(0f, 0f, 0f);
+                        augmentation.transform.localRotation = Quaternion.identity;
+                        augmentation.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+                        augmentation.gameObject.SetActive(true);
+                        augmentation.name = tb.TrackableName;
+                    }
+                    else
+                    {
+                        Debug.Log("<color=yellow>Warning: No augmentation object specified for: " + tb.TrackableName + "</color>");
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("<color=yellow>Failed to load dataset: '" + dataSetName + "'</color>");
+        }
+    }
+    void Update() {
+        IEnumerable<TrackableBehaviour> tbs = TrackerManager.Instance.GetStateManager().GetTrackableBehaviours();
+        foreach (TrackableBehaviour tb in tbs) {
+            Transform board = tb.gameObject.transform.GetChild(0);
+            Text _currencyText = board.GetChild(0).gameObject.GetComponent<Text>();
+            _currencyText.text = board.name;
+        }
+
+
+    }
+    void doOnce() {
+        XMLMANGER.readExampleXml();
+        List<POI> Pois =  XMLMANGER.modelList.GetRange(0, XMLMANGER.modelList.Count);
+        //List<string> lstNew = lstTest.GetRange(0, 3);
+        foreach (POI point in Pois)
+        {
+            Debug.Log(point.Name);
+        }
+
+    }
+}
---
+++ changeText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Text _currencyText = GameObject.Find("Canvas").transform.GetChild(0).gameObject.GetComponent<Text>();
+        Debug.Log(GameObject.Find("Canvas").transform.GetChild(0).gameObject.name);
+        //Text _currencyText = GameObject.Find("Canvas").GetComponentInChildren<Text>();
+        _currencyText.text = "This is a test!";
+    }
+}
