30dde751dcacd68b46176bff5ab5ff0431219d72
uclhenry <yuhangxuhenry@gmail.com>
2017-11-28 21:03:01 +0000

    add sphere

--- DynamicDataSetLoader.cs
+++ DynamicDataSetLoader.cs
@@ -13,8 +13,9 @@ public class DynamicDataSetLoader : MonoBehaviour
     private string _result;
     //public bool once = false;
     // specify these in Unity Inspector
-    public GameObject augmentationObject = null;  // you can use teapot or other object
-    public string dataSetName = "";  //  Assets/StreamingAssets/QCAR/DataSetName
+    public GameObject augmentationObject = null;  // you can use teapot or other object
+    public GameObject secondObject = null;
+    public string dataSetName = "";  //  Assets/StreamingAssets/QCAR/DataSetName
     public bool once = false;
     public static List<POI> Pois = null;
     // Use this for initialization
@@ -34,7 +35,7 @@ public class DynamicDataSetLoader : MonoBehaviour
         SetUpPois(elements);
         //createAlltext();
     }
-     public static void SetUpPois(IEnumerable<XElement> elements)
+    public static void SetUpPois(IEnumerable<XElement> elements)
     {
         Pois = new List<POI>();
 
@@ -51,20 +52,16 @@ public class DynamicDataSetLoader : MonoBehaviour
             point.TargetWidth = ele.Element("TargetWidth").Value;
             point.userId = ele.Element("userId").Value;
             String s = ele.Element("Latitude").Value;
-            
-            s = decimal.Round(decimal.Parse(s), 2).ToString();
+
+            s = decimal.Round(decimal.Parse(s), 6).ToString();
             point.Latitude = s;
-            s= ele.Element("Longitude").Value;           
-            s = decimal.Round(decimal.Parse(s), 2).ToString();
+            s = ele.Element("Longitude").Value;
+            s = decimal.Round(decimal.Parse(s), 6).ToString();
             point.Longitude = s;
             //point.Latitude = Convert.ToDouble(ele.Element("Latitude").Value);
             //point.Longitude = Convert.ToDouble(ele.Element("Longitude").Value);
             point.SimilarityThreshold = Convert.ToSingle(ele.Element("SimilarityThreshold").Value);
             Pois.Add(point);
-
-            //Debug.Log(ele.Element("blog").Value);
-            //model.BookPrice = Convert.ToDouble(ele.Element("price").Value);
-
         }
 
     }
@@ -82,8 +79,8 @@ public class DynamicDataSetLoader : MonoBehaviour
 
             if (!objectTracker.ActivateDataSet(dataSet))
             {
-                // Note: ImageTracker cannot have more than 100 total targets activated
-                Debug.Log("<color=yellow>Failed to Activate DataSet: " + dataSetName + "</color>");
+                // Note: ImageTracker cannot have more than 100 total targets activated
+                Debug.Log("<color=yellow>Failed to Activate DataSet: " + dataSetName + "</color>");
             }
 
             if (!objectTracker.Start())
@@ -108,8 +105,8 @@ public class DynamicDataSetLoader : MonoBehaviour
 
                     if (augmentationObject != null)
                     {
-                        // instantiate augmentation object and parent to trackable
-                        GameObject augmentation = (GameObject)GameObject.Instantiate(augmentationObject);
+                        // instantiate augmentation object and parent to trackable
+                        GameObject augmentation = (GameObject)GameObject.Instantiate(augmentationObject);
                         augmentation.transform.parent = tb.gameObject.transform;
                         augmentation.transform.localPosition = new Vector3(0f, 0f, 0f);
                         Quaternion target = Quaternion.Euler(90f, 0, 0);
@@ -118,11 +115,17 @@ public class DynamicDataSetLoader : MonoBehaviour
                         augmentation.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
                         augmentation.gameObject.SetActive(true);
                         augmentation.name = tb.TrackableName;
+
+                        GameObject second = (GameObject)GameObject.Instantiate(secondObject);
+                        second.transform.parent = tb.gameObject.transform;
+                        //second.transform.localPosition = new Vector3(0f, 0f, 0f);
+                        second.gameObject.SetActive(true);
                     }
                     else
                     {
                         Debug.Log("<color=yellow>Warning: No augmentation object specified for: " + tb.TrackableName + "</color>");
                     }
+ 
                 }
             }
         }
@@ -131,52 +134,45 @@ public class DynamicDataSetLoader : MonoBehaviour
             Debug.LogError("<color=yellow>Failed to load dataset: '" + dataSetName + "'</color>");
         }
     }
-    void Update() {
-        //if (once == false) {
-        //    doOnce();
-        //    once = true;
-        //}
-        
+    void Update()
+    {
         IEnumerable<TrackableBehaviour> tbs = TrackerManager.Instance.GetStateManager().GetTrackableBehaviours();
-        foreach (TrackableBehaviour tb in tbs) {
+        foreach (TrackableBehaviour tb in tbs)
+        {
             Transform board = tb.gameObject.transform.GetChild(0);
             Text _currencyText = board.GetChild(0).gameObject.GetComponent<Text>();
             _currencyText.text = board.name;
             POI p = null;
-
-            foreach (POI point in Pois) {
-                if (point.Name == board.name) {
-                    p = point;
-                }
-                if (p != null)
+            if (Pois != null) {
+                foreach (POI point in Pois)
                 {
-                    Text t2 = board.GetChild(1).gameObject.GetComponent<Text>();
-                    Text t3 = board.GetChild(2).gameObject.GetComponent<Text>();
-                    Text t4 = board.GetChild(3).gameObject.GetComponent<Text>();
-                    Text t5 = board.GetChild(4).gameObject.GetComponent<Text>();
-                    Text t6 = board.GetChild(5).gameObject.GetComponent<Text>();
-                    Text t7 = board.GetChild(6).gameObject.GetComponent<Text>();
-                    //find the poi with name = board.name
-                    //get the location and other infomation
-                    t2.text = "locate at " + p.Latitude + " ," + p.Longitude;
-                    t3.text = "id : " + p.Id;
-                    t4.text = "user id : " + p.userId;
-                    t5.text = "Target Height :" + p.TargetHeight;
-                    t6.text = "latitude : " + p.Latitude;
-                    t7.text = "longitude :" + p.Longitude;
-
-
+                    if (point.Name == tb.TrackableName)
+                    {
+                        p = point;
+                        Debug.Log("point" + point.Name);
+                        Text t2 = board.GetChild(1).gameObject.GetComponent<Text>();
+                        Text t3 = board.GetChild(2).gameObject.GetComponent<Text>();
+                        Text t4 = board.GetChild(3).gameObject.GetComponent<Text>();
+                        Text t5 = board.GetChild(4).gameObject.GetComponent<Text>();
+                        Text t6 = board.GetChild(5).gameObject.GetComponent<Text>();
+                        Text t7 = board.GetChild(6).gameObject.GetComponent<Text>();
+                        //find the poi with name = board.name
+                        //get the location and other infomation
+                        t2.text = "locate at " + p.Latitude + " ," + p.Longitude;
+                        t3.text = "id : " + p.Id;
+                        t4.text = "user id : " + p.userId;
+                        t5.text = "Target Height :" + p.TargetHeight;
+                        t6.text = "latitude : " + p.Latitude;
+                        t7.text = "longitude :" + p.Longitude;
+                    }          
                 }
-
             }
-
-
-
         }
 
 
     }
-    IEnumerator LoadXML(){
+    IEnumerator LoadXML()
+    {
         string sPath = "";
         if (Application.platform == RuntimePlatform.WindowsEditor) sPath = "file://" + Application.streamingAssetsPath + "/VisAge.xml";
         else if (Application.platform == RuntimePlatform.Android)
@@ -189,7 +185,5 @@ public class DynamicDataSetLoader : MonoBehaviour
         _result = www.text;
         readExampleXml();
     }
-    void doOnce() {
 
-    }
 }
