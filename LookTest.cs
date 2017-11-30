6ac152f0648af99ccf14faffaa62b307c0d6960c
uclhenry <yuhangxuhenry@gmail.com>
2017-11-30 01:11:51 +0000

    target position or direction vector

---
+++ LookTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookTest : MonoBehaviour {
+    public Transform target;
+    public float x;
+    public float y;
+    public float z;
+
+    void Update()
+    {
+        // Point the object at the world origin
+        transform.LookAt(new Vector3(x, y, z));
+    }//Vector3.zero target 
+}
